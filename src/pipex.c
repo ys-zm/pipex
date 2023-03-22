9558d62dab26e8d8610418944031344c0f8717c6
Yesim Zaim <yzaim@f0r2s9.codam.nl>
2023-03-22 21:22:17 +0100

    update

--- src/pipex.c
+++ src/pipex.c
@@ -1,2 +1,17 @@
 //TO RESEARCH
 // pipe, fork, dup2, execve
+
+#include <unistd.h>
+//pid, when in the main process is not 0. when in the child process is 0.
+//copies the main in another locatino in memory.
+//wait() returns the process id of the thing you waited for
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid)
+		//parent process
+	else
+		//child process. id = 0;
+}
